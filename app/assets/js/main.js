39987391e0e893c1a0fa6341028477579577bf40
Jejomar <jejomardorongon@gmail.com>
2017-09-09 20:41:56 +0800

    Finished part 4 of js

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -41,9 +41,27 @@ $(document).ready(function() {
         this.showNext = function(next) {
             var wrapper = next.find('.wrapper');
             wrapper.fadeIn('500', function() {
-
+                manageOptions(next);
             });
-        }
+        };
+
+        this.manageOptions = function(next) {
+            var options = next.find('.options');
+            var children = options.find('div');
+            var counter = 0;
+
+            children.each(function(i, elem) {
+                $(elem).delay(counter).fadeIn(300);
+                counter += 500;
+            });
+
+            children.on('click', function() {
+                children.removeClass('active');
+                next.addClass('valid'); 
+                $(this).addClass('active');
+            });
+
+        };
 
         loadQuiz();
 
